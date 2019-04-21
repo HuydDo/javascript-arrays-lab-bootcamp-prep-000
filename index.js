dc33c38fb39da242b161dde48b8c8b8e90a53dd3
Huy do <huyddo40@gmail.com>
2019-04-21 03:22:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function destructivelyAppendKitten(name){
 }
 
 function destructivelyPrependKitten(name){
-  kittens.unshift(name)
-  return kittens
+  var prepend = kittens.unshift(name)
+  return prepend
 }
